5c6e6fd20c4fc70b016e6cde565b1a868396ac23
ManuelDittmar <manuel.dittmar@camunda.com>
2021-12-26 21:05:13 +0100

    TaskTypeDefinition Test

---
+++ bpmn-pragmatic-check/src/test/java/edu/kit/TaskTypeDefinitionTest.java
@@ -0,0 +1,68 @@
+package edu.kit;
+
+import edu.kit.domain.qualityMetrics.CompleteLabeling;
+import edu.kit.domain.qualityMetrics.TaskTypeDefinition;
+import org.camunda.bpm.model.bpmn.Bpmn;
+import org.camunda.bpm.model.bpmn.instance.Process;
+import org.camunda.bpm.model.xml.ModelInstance;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class TaskTypeDefinitionTest {
+
+    String resourcePath = Paths.get("src","test","resources").toFile().getAbsolutePath();
+    TaskTypeDefinition taskTypeDefinition;
+
+    @BeforeAll
+    public void init() throws FileNotFoundException {
+        ModelInstance modelInstance = Bpmn.readModelFromStream(new FileInputStream(resourcePath +"\\TaskTypeDefinitionTest.bpmn"));
+        List<Process> processList = new ArrayList(modelInstance.getModelElementsByType(Process.class));
+        taskTypeDefinition = new TaskTypeDefinition(processList.get(0));
+    }
+
+    @Test
+    public void hasOutlierNoneTaskType() {
+        assertEquals(true, taskTypeDefinition.getOutliers().contains("NoneTypeTask"));
+        assertEquals(true, taskTypeDefinition.getOutliers().contains("NoneTaskTypeSubprocess"));
+    }
+
+    @Test
+    public void hasOutlierScriptTask() {
+        assertEquals(true, taskTypeDefinition.getOutliers().contains("ScriptTask"));
+    }
+
+    @Test
+    public void hasOutlierCollapsedSubprocess() {
+        assertEquals(true, taskTypeDefinition.getOutliers().contains("CollapsedSubprocess"));
+    }
+
+    @Test
+    public void hasOutlierManualTask() {
+        assertEquals(true, taskTypeDefinition.getOutliers().contains("ManualTask"));
+    }
+
+    @Test
+    public void hasOutlierLoopMarker() {
+        assertEquals(true, taskTypeDefinition.getOutliers().contains("LoopMarkerServiceTask"));
+    }
+
+    @Test
+    public void hasNotOutlierServiceTask() {
+        assertEquals(false, taskTypeDefinition.getOutliers().contains("ServiceTaskNoMarker"));
+    }
+
+    @Test
+    public void hasNotOutlierExpandedSubprocess() {
+        assertEquals(false, taskTypeDefinition.getOutliers().contains("ExpandedSubProcess"));
+    }
+}
