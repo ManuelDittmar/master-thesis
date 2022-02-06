47af2e0643209fbecac1ee1e8e29daa23fa867e5
ManuelDittmar <manuel.dittmar@camunda.com>
2022-02-06 15:33:06 +0100

    Deadlock Detection

--- bpmn-pragmatic-check/src/main/java/edu/kit/domain/quality/BehavioralErrors.java
+++ bpmn-pragmatic-check/src/main/java/edu/kit/domain/quality/BehavioralErrors.java
@@ -1,10 +1,18 @@
 package edu.kit.domain.quality;
 
+import edu.kit.domain.Outlier;
+import org.camunda.bpm.model.bpmn.instance.FlowNode;
+import org.camunda.bpm.model.bpmn.instance.ParallelGateway;
 import org.camunda.bpm.model.bpmn.instance.Process;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 
-
-//@Component
+@Component
 public class BehavioralErrors extends ProcessQualityCriteria {
 
     public BehavioralErrors() {
@@ -14,11 +22,35 @@ public class BehavioralErrors extends ProcessQualityCriteria {
         super(process);
     }
 
-    // TODO Check if Start Events in Subprocess will also be taken into account
 
     @Override
     public void init() {
+        // Synchronizing Gateway
+        List<ParallelGateway> parallelGatewayList = new ArrayList<>(process.getChildElementsByType(ParallelGateway.class)).stream()
+                .filter(parallelGateway -> parallelGateway.getIncoming().size()>1).collect(Collectors.toList());
+        parallelGatewayList.forEach(parallelGateway -> {
+            int incomingSequenceFlows = parallelGateway.getIncoming().size();
+            boolean foundSplitting = false;
+            long parallelSplits = getAllPreviousFlowNodes(parallelGateway).stream()
+                    .filter(flowNode -> hasSplittingBehavior(flowNode,incomingSequenceFlows)).count();
+            if(parallelSplits > 0) {
+                foundSplitting = true;
+            }
+            if(!foundSplitting) {
+                outliers.add(new Outlier(parallelGateway.getId(), Set.of("Potential Deadlock")));
+            }
+        });
+        setCalculatedScore(parallelGatewayList.size());
+    }
 
+    public boolean hasSplittingBehavior(FlowNode flowNode,int incomingFlows) {
+        if(flowNode.getOutgoing().size() < incomingFlows) {
+            return false;
+        }
+        boolean isSplittingActivity = flowNode.getElementType().getBaseType().getTypeName().equals("activity");
+        boolean isSplittingTask =  flowNode.getElementType().getBaseType().getTypeName().equals("task");
+        boolean isParallelGateway = flowNode.getElementType().getInstanceType().getSimpleName().equals("ParallelGateway");
+        return (isSplittingActivity || isSplittingTask || isParallelGateway);
     }
 
 }
