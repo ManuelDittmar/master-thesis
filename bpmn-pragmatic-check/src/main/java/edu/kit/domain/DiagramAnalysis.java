590d1047e73dbe53b8e6b136ac00bdb83201a5d6
ManuelDittmar <manuel.dittmar@camunda.com>
2022-04-20 21:23:47 +0200

    Update DiagramAnalysis.java

--- bpmn-pragmatic-check/src/main/java/edu/kit/domain/DiagramAnalysis.java
+++ bpmn-pragmatic-check/src/main/java/edu/kit/domain/DiagramAnalysis.java
@@ -3,65 +3,64 @@ package edu.kit.domain;
 import edu.kit.domain.common.CriteriaType;
 import edu.kit.domain.quality.DiagramQualityCriteria;
 import edu.kit.service.ProcessAnalysisService;
+import java.util.ArrayList;
+import java.util.List;
 import org.camunda.bpm.model.bpmn.instance.Process;
 import org.camunda.bpm.model.xml.ModelInstance;
 
-import java.util.ArrayList;
-import java.util.List;
-
-/**
- * Diagram Analysis consists of Diagram Quality Criteria &
- * ProcessAnalysis for each Pool
- */
+//Diagram Analysis consists of Diagram Quality Criteria & ProcessAnalysis for each Pool
 
 
 public class DiagramAnalysis {
 
-    private boolean mandatoryCriteriaNotMet;
-    private List<DiagramQualityCriteria> diagramQualityCriteriaList;
-    private List<ProcessAnalysis> processAnalysisList;
+  private boolean mandatoryCriteriaNotMet;
+  private List<DiagramQualityCriteria> diagramQualityCriteriaList;
+  private List<ProcessAnalysis> processAnalysisList;
 
-    public DiagramAnalysis(ModelInstance modelInstance, ProcessAnalysisService processAnalysisService, List<DiagramQualityCriteria> diagramQualityCriteriaList) {
-        this.diagramQualityCriteriaList = new ArrayList<>();
-        this.mandatoryCriteriaNotMet = false;
-        if(diagramQualityCriteriaList!=null) {
-            diagramQualityCriteriaList.forEach(diagramCriteria -> {
-                DiagramQualityCriteria criteria = diagramCriteria.createInstance(modelInstance);
-                this.diagramQualityCriteriaList.add(criteria);
-                if(criteria.getCriteriaType().equals(CriteriaType.MANDATORY) && criteria.getScore() < 1.0) {
-                    this.mandatoryCriteriaNotMet = true;
-                }
-            });
+  public DiagramAnalysis(ModelInstance modelInstance, ProcessAnalysisService processAnalysisService,
+      List<DiagramQualityCriteria> diagramQualityCriteriaList) {
+    this.diagramQualityCriteriaList = new ArrayList<>();
+    this.mandatoryCriteriaNotMet = false;
+    if (diagramQualityCriteriaList != null) {
+      diagramQualityCriteriaList.forEach(diagramCriteria -> {
+        DiagramQualityCriteria criteria = diagramCriteria.createInstance(modelInstance);
+        this.diagramQualityCriteriaList.add(criteria);
+        if (criteria.getCriteriaType().equals(CriteriaType.MANDATORY)
+            && criteria.getScore() < 1.0) {
+          this.mandatoryCriteriaNotMet = true;
         }
-
-        List<Process> processes = (List<Process>) modelInstance.getModelElementsByType(Process.class);
-        this.processAnalysisList = new ArrayList<>();
-        for (Process process : processes) {
-            ProcessAnalysis processAnalysis = processAnalysisService.analyseProcess(process);
-            this.processAnalysisList.add(processAnalysis);
-            if(processAnalysis.isMandatoryCriteriaNotMet()) {
-                this.mandatoryCriteriaNotMet = true;
-            }
-        }
+      });
     }
 
-    public boolean isMandatoryCriteriaNotMet() {
-        return mandatoryCriteriaNotMet;
+    List<Process> processes = (List<Process>) modelInstance.getModelElementsByType(Process.class);
+    this.processAnalysisList = new ArrayList<>();
+    for (Process process : processes) {
+      ProcessAnalysis processAnalysis = processAnalysisService.analyseProcess(process);
+      this.processAnalysisList.add(processAnalysis);
+      if (processAnalysis.isMandatoryCriteriaNotMet()) {
+        this.mandatoryCriteriaNotMet = true;
+      }
     }
+  }
 
-    public void setProcessAnalysisList(List<ProcessAnalysis> processAnalysisList) {
-        this.processAnalysisList = processAnalysisList;
-    }
+  public boolean isMandatoryCriteriaNotMet() {
+    return mandatoryCriteriaNotMet;
+  }
 
-    public void setDiagramQualityCriteriaList(List<DiagramQualityCriteria> diagramQualityCriteriaList) {
-        this.diagramQualityCriteriaList = diagramQualityCriteriaList;
-    }
+  public void setProcessAnalysisList(List<ProcessAnalysis> processAnalysisList) {
+    this.processAnalysisList = processAnalysisList;
+  }
 
-    public List<ProcessAnalysis> getProcessAnalysisList() {
-        return processAnalysisList;
-    }
+  public void setDiagramQualityCriteriaList(
+      List<DiagramQualityCriteria> diagramQualityCriteriaList) {
+    this.diagramQualityCriteriaList = diagramQualityCriteriaList;
+  }
 
-    public List<DiagramQualityCriteria> getDiagramQualityCriteriaList() {
-        return diagramQualityCriteriaList;
-    }
+  public List<ProcessAnalysis> getProcessAnalysisList() {
+    return processAnalysisList;
+  }
+
+  public List<DiagramQualityCriteria> getDiagramQualityCriteriaList() {
+    return diagramQualityCriteriaList;
+  }
 }
